196e6b6aa71943abc4530e3fcf8471e0639d9484
PREETI2096 <preetirg96@gmail.com>
2023-02-12 21:51:22 +0530

    Added : repeat sad mood

--- moodanalyser/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ moodanalyser/src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -2,13 +2,17 @@ package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
 
-	public String moodAnalyser(String message)
-	    {
-	        if (message.contains("Sad"))
-	            return "SAD";
-	        else
-	        return "HAPPY";
-	    }
+	 private String message;
+    public void MoodAnalyser(String message) {
+        this.message = message;
+    }
+    public String moodAnalyser(String message) {
+        if (message.contains("Sad")) {
+            return "SAD";
+        } else {
+            return "SAD";
+        }
+    }
 	 public static void main(String[] args) {
 		MoodAnalyser mood = new MoodAnalyser ();
 	    MoodAnalyser m = new MoodAnalyser();
